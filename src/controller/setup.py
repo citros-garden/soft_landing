86c2c3c5d89cb6a348c2624ab306568743a41d13
Iftach Naftaly <58637596+iftahnaf@users.noreply.github.com>
2023-03-19 11:12:43 +0200

    Revert "Controller"

--- src/controller/setup.py
+++ src/controller/setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-from glob import glob
-import os
 
 package_name = 'controller'
 
@@ -12,18 +10,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ros',
-    maintainer_email='orr@lulav.space',
+    maintainer='orrlulavspace',
+    maintainer_email='orrlulavspace@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'controller = controller.controller:main'
         ],
     },
 )
