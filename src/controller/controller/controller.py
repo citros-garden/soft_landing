175c9af7819fcf7f09a7ac3db14fe6236be915c0
orr <orr@lulav.space>
2023-03-19 10:58:30 +0200

    working on the controller

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 import time
+import pyvectorguidance
 
 from std_msgs.msg import Float64MultiArray  
 
@@ -13,6 +14,7 @@ class Controller(Node):
         self.state_sub = self.create_subscription(Float64MultiArray, '/dynamics/state', self.state_cb, 10)
        
         self.dt = 0.1 
+        self.vg = pyvectorguidance.VectorGuidance()
         # define parameters        
 
         self.declare_parameter('setpoint_r_x')
@@ -21,10 +23,12 @@ class Controller(Node):
         self.declare_parameter('setpoint_v_x')
         self.declare_parameter('setpoint_v_y')
         self.declare_parameter('setpoint_v_z')
-        
+        self.declare_parameter('g')
 
         self.u_msg = Float64MultiArray() #[rx ,ry ,rz ,vx,vy,vz]
         self.state = Float64MultiArray() #[rx ,ry ,rz ,vx,vy,vz]
+        self.r = Float64MultiArray()
+        self.v = Float64MultiArray()
 
 
         time.sleep(1)
@@ -35,13 +39,26 @@ class Controller(Node):
         self.setpoint_v_x = self.get_parameter('setpoint_v_x').get_parameter_value().double_value
         self.setpoint_v_y = self.get_parameter('setpoint_v_y').get_parameter_value().double_value
         self.setpoint_v_z = self.get_parameter('setpoint_v_z').get_parameter_value().double_value
+        self.g = self.get_parameter('g').get_parameter_value().double_value
+
 
         time.sleep(1)
         self.get_logger().info(f"Starting controller with the setpoint r = {[self.setpoint_r_x,self.setpoint_r_y,self.setpoint_r_z]} and v = {[self.setpoint_v_x,self.setpoint_v_y,self.setpoint_v_z]}")
 
 
     def state_cb(self, msg):
-        self.state = msg.data
+
+
+        self.r.data = [self.state_sub[0],self.state_sub[1],self.state_sub[2]] #[rx ,ry ,rz]
+        self.v.data =[self.state_sub[3],self.state_sub[4],self.state_sub[5]] #[vx,vy,vz]
+
+
+        tgo = self.vg.soft_landing_tgo_lq(self.r, self.v, 0, self.g)
+        u = self.vg.soft_landing_controller_lq(self.r, self.v, tgo, self.g)
+        
+        self.u_msg.data = u
+        self.u_pub.publish(self.u_msg)
+
 
    
 def main(args=None):
