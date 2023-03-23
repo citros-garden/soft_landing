2953a5e0d74847a4e24c2f4e72f57e783dd8035f
orr <orr@lulav.space>
2023-03-23 09:36:26 +0000

    fixing the controllrer and update the Dockerfile

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -21,13 +21,9 @@ class Controller(Node):
         self.declare_parameter('setpoint_v_x', 0.0)
         self.declare_parameter('setpoint_v_y', 0.0)
         self.declare_parameter('setpoint_v_z', 0.0)
-        self.declare_parameter('g', 0.0) 
-        self.declare_parameter('um', 0.0)
-        
-        # self.g = [0,0,0]
-        # self.um = 0
-
-
+        self.declare_parameter('g', 1.62) 
+        self.declare_parameter('um', 20.0)
+        self.declare_parameter('e',0.02)
         self.u_msg = Float64MultiArray() #[rx ,ry ,rz ,vx,vy,vz]
         self.state = [] #[rx ,ry ,rz ,vx,vy,vz]
         self.r = []
@@ -43,30 +39,28 @@ class Controller(Node):
         self.setpoint_v_z = self.get_parameter('setpoint_v_z').get_parameter_value().double_value
         self.g = [0,0,self.get_parameter('setpoint_v_z').get_parameter_value().double_value]
         self.um = self.get_parameter('um').get_parameter_value().double_value
-
-        time.sleep(1)
-        # self.get_logger().info(f"Starting controller with the setpoint r = {[self.setpoint_r_x,self.setpoint_r_y,self.setpoint_r_z]} and v = {[self.setpoint_v_x,self.setpoint_v_y,self.setpoint_v_z]}")
-
+        self.e = self.get_parameter('e').get_parameter_value().double_value
+        
     def state_cb(self, msg):
         self.r_tmp = msg.data[0:3]
         self.v_tmp = msg.data[3:6]
         self.r =np.array( [self.r_tmp[0],self.r_tmp[1],self.r_tmp[2]])
         self.v =np.array( [self.v_tmp[0],self.v_tmp[1],self.v_tmp[2]])
-        # self.get_logger().info(f"msg = {msg.data}, r= {self.r} ,v= {self.v}  ",throttle_duration_sec=0.5)
-        # tgo = self.vg.soft_landing_tgo_lq(self.r, self.v, self.um, self.g) #positiv value
-
         norm_r = np.linalg.norm(self.r)
         norm_v = np.linalg.norm(self.v)
-        tgo = norm_r/(norm_v+0.01)
-        self.get_logger().info(f"tgo is = {tgo}, r= {self.r},v= {self.v}",throttle_duration_sec=0.5)
-
-        try:
-            u = self.vg.soft_landing_controller_lq(self.r, self.v,tgo, self.g)
-        except:
-            tgo = 0.0
-            self.get_logger().info(f"tgo is = {tgo}, r= {self.r},v= {self.v}",throttle_duration_sec=0.5)
-            exit()
+        tgo = self.vg.soft_landing_tgo_lq(self.r,self.v,self.um,self.g)[0]
+        self.get_logger().info(f"tgo is = {tgo:.3f}, r=  [{self.r[0]:.3f}, {self.r[1]:.3f}, {self.r[2]:.3f}],v= [{self.v[0]:.3f}, {self.v[1]:.3f}, {self.v[2]:.3f}]",throttle_duration_sec=0.1)
         
+        if tgo > self.e:
+            u = -self.vg.soft_landing_controller_lq(self.r, self.v,tgo, self.g)
+        else:
+            u = [0,0,0]
+            # self.miss_distance = 
+            # self.miss_velocity = 
+
+            # self.get_logger().info(f"The miss distance is: [{}] and the miss velocity is: [{}]")
+            exit()
+            
         u = [float(x) for x in u]
         self.u_msg.data = u
         self.u_pub.publish(self.u_msg)
--- src/dynamics/dynamics/dynamics.py
+++ src/dynamics/dynamics/dynamics.py
@@ -8,61 +8,45 @@ class Dynamics(Node):
 
     def __init__(self):
         super().__init__('dynamics')
-        
         self.state_pub = self.create_publisher(Float64MultiArray , '/dynamics/state', 10)
         self.u_sub = self.create_subscription(Float64MultiArray , '/controller/command', self.u_cb, 10)
-
         self.dt = 0.01  # seconds
         # define parameters
 
-        self.declare_parameter('g_x')
-        self.declare_parameter('g_y')
-        self.declare_parameter('g_z')
-
-        self.declare_parameter('r_x0') #r0
-        self.declare_parameter('r_y0')
-        self.declare_parameter('r_z0')
-        
-        self.declare_parameter('v_x0')
-        self.declare_parameter('v_y0')
-        self.declare_parameter('v_z0')
-        
-
+        self.declare_parameter('g_x',0.0)
+        self.declare_parameter('g_y',0.0)
+        self.declare_parameter('g_z',1.62)
+        self.declare_parameter('r_x0',0.0) #r0
+        self.declare_parameter('r_y0',0.0)
+        self.declare_parameter('r_z0',100.0)
+        self.declare_parameter('v_x0',0.0)
+        self.declare_parameter('v_y0',0.0)
+        self.declare_parameter('v_z0',0.0)
         self.u_cmd =[0,0,0] #[ux ,uy ,uz ]
         self.state_msg = Float64MultiArray() #[rx ,ry ,rz ,vx,vy,vz]
-        
 
         time.sleep(1)
 
         self.g_x = self.get_parameter('g_x').get_parameter_value().double_value
         self.g_y = self.get_parameter('g_y').get_parameter_value().double_value
         self.g_z = self.get_parameter('g_z').get_parameter_value().double_value
-
         self.r_x = self.get_parameter('r_x0').get_parameter_value().double_value
         self.r_y = self.get_parameter('r_y0').get_parameter_value().double_value
         self.r_z = self.get_parameter('r_z0').get_parameter_value().double_value
-        
         self.v_x = self.get_parameter('v_x0').get_parameter_value().double_value
         self.v_y = self.get_parameter('v_y0').get_parameter_value().double_value
         self.v_z = self.get_parameter('v_z0').get_parameter_value().double_value
        
-
         time.sleep(1)
-
         self.timer = self.create_timer(self.dt, self.step)
 
     def u_cb(self, msg):
         self.u_cmd = msg.data #return list
 
     def solve_ode(self):
-
-
         self.v_x += ((self.u_cmd[0] - self.g_x) * self.dt) # Integral(u - g)+v0 = v
         self.v_y += ((self.u_cmd[1] - self.g_y) * self.dt)
-        self.v_z += ((self.u_cmd[2] - self.g_z) * self.dt)
-       
-            
-
+        self.v_z += ((self.u_cmd[2] - self.g_z) * self.dt)          
         self.r_x += (self.v_x * self.dt) # Integral(v)+r0 = x
         self.r_y += (self.v_y * self.dt)
         self.r_z += (self.v_z * self.dt)
@@ -74,17 +58,10 @@ class Dynamics(Node):
     def step(self):
 
         self.solve_ode()
-
-
-        self.get_logger().info(f"pose: {[self.r_x , self.r_y ,self.r_z]} velocity {[self.v_x , self.v_y ,self.v_z]}, controller: {self.u_cmd}", throttle_duration_sec=0.5)
-
         self.state_msg.data = [self.r_x , self.r_y ,self.r_z ,self.v_x ,self.v_y, self.v_z]
         self.state_pub.publish(self.state_msg)
-        
         if self.r_z == 0.0 : # end when we leand
-            self.get_logger().info(f"r: {[self.r_x , self.r_y ,self.r_z]} velocity {[self.v_x , self.v_y ,self.v_z]}, controller: {self.u_cmd[1]}", throttle_duration_sec=0.5)
             exit()
-        
         
 def main(args=None):
     rclpy.init(args=args)
