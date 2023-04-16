56f4263a3286b798cf7dd021222ce10fbf819245
Iftach Naftaly <iftahnaf@gmail.com>
2023-04-16 15:16:53 +0000

    fixing installation script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,6 +5,6 @@ cd $WORKSPACE_DIR
 
 git submodule update --init --recursive
 
-python3 -m pip install -e /home/lulav/dev/citros-garden/soft_landing/src/lopt_dynamics/resource/lopt/
+pip install -e /workspaces/soft_landing/src/lopt_dynamics/resource/lopt/
 
 echo "Done installing, ready to develop!"
